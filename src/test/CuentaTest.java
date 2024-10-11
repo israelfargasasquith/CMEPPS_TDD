0892e723c2945245d9400129f3bfba77ccf9b394
israe <israe@Raeliss>
2024-10-11 10:27:58 +0200

    Primer paso de CuentaTest

---
+++ src/test/CuentaTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+	
+	Cuenta cuenta;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	cuenta = new Cuenta(0);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		cuenta.ingresar(1);
+		assertEquals(1,cuenta.getSaldo());
+	}
+
+}
